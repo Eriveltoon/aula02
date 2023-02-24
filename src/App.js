79d7ca56df877debe7b92fc3f84baca4e46585fd
ERIVELTON DOS SANTOS BARRETO <erivelton.sbarreto@CAS474696C148A.local>
2023-02-24 19:29:10 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Noticia from "./components/noticia/noticia";
 
 function App() {
+
+  const [noticias, setNoticias] = React.useState([]);
+
+  function carregar(){
+  setNoticias([
+    {
+      imagem: "https://picsum.photos/200/200?grayscale" ,
+      titulo:"Titulo 1",
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled "
+    },
+    {
+      imagem: "https://picsum.photos/200/200" ,
+      titulo:"Titulo 2",
+      texto: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled "
+    }
+  ])};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    noticias.length >= 2 ?
+    <div className="container">
+      <Noticia imagem={noticias[0].imagem} titulo={noticias[0].titulo} texto={noticias[0].texto}/>
+      <Noticia imagem={noticias[1].imagem} titulo={noticias[1].titulo} texto={noticias[1].texto}/>
+    </div>
+    :
+    <div className="container">
+      <button onClick={carregar}>Carregar Noticias</button>
+      <div>Sem noticias</div>
     </div>
   );
 }
---
+++ src/components/noticia/texto.js
@@ -0,0 +1,11 @@
+function Texto(props){
+    return (
+        <>
+            <h1>{props.titulo}</h1>
+            <p>{props.texto}</p>
+        </>
+        
+    );
+}
+
+export default Texto;
